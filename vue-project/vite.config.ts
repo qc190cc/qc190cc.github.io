6d5d44658d0810563f046fba68733d12703f5d81
qc190cc <qc190c@qq.com>
2025-01-14 12:24:42 +0800

    2

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -15,4 +15,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    emptyOutDir: true,
+    outDir: "../dist",
+    minify: "esbuild"
+  }
 })
